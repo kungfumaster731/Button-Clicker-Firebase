ad97682a97e97d8e4168881918de28df974ac30a
Brandon Chow <53826472+kungfumaster731@users.noreply.github.com>
2021-05-23 14:54:07 -0400

    d

--- ButtonClicker/app/src/main/java/com/example/buttonclicker/MainActivity.java
+++ ButtonClicker/app/src/main/java/com/example/buttonclicker/MainActivity.java
@@ -3,6 +3,17 @@ package com.example.buttonclicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+
+
+        Button send = (Button) findViewById(R.id.button);
+        EditText textbox = (EditText) findViewById(R.id.TextBox);
+        TextView text = (TextView) findViewById(R.id.textView2);
+
+        send.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                myRef.setValue(textbox.getText().toString());
+            }
+        });
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                text.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
+
     }
 }
